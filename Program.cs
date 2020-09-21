c5b3b1278490635c8c7e971831555f3119fedd45
David Jefferies <david@paperseven.com>
2020-09-22 00:38:21 +0100

    Update formatting

--- Program.cs
+++ Program.cs
@@ -6,27 +6,54 @@ namespace MutableString
     {
         static void Main(string[] args)
         {
+            Usage();
+            Limitations();
+        }
+
+        static void Usage()
+        {
+            Console.WriteLine("Usage");
             MutableString mutableString1 = new MutableString("ABCDEF");
             MutableString mutableString2 = new MutableString("AAAAAA");
 
             Console.WriteLine($"{mutableString1}=={mutableString2} is {mutableString1 == mutableString2}");
             // Displays ABCDEF==AAAAAA is False
+
             mutableString2.SetSubString(3, "DEF");
             // Displays ABCDEF==AAADEF is False
+
             Console.WriteLine($"{mutableString1}=={mutableString2} is {mutableString1 == mutableString2}");
             mutableString2.SetSubString(0, "ABC", false);
-            // Displays ABCDEF==ABCDEF is True
             Console.WriteLine($"{mutableString1}=={mutableString2} is {mutableString1 == mutableString2}");
+            // Displays ABCDEF==ABCDEF is True
+
+            Console.WriteLine($"{mutableString2} length is {mutableString2.Length} capacity is {mutableString2.Capacity}");
+            // Displays ABCDEF length is 6 capacity is 6
+
             mutableString2.SetString("ABC");
-            Console.WriteLine($"{mutableString2} length is {mutableString2.Length}");
+            Console.WriteLine($"{mutableString2} length is {mutableString2.Length} capacity is {mutableString2.Capacity}");
             // Displays ABC length is 3
+
             MutableString mutableString3 = new MutableString(32);
             mutableString3.SetString("12345678");
             Console.WriteLine($"{mutableString3} length is {mutableString3.Length} capacity is {mutableString3.Capacity}");
             // Displays 12345678 length is 8 capacity is 32
+
             mutableString3.SetSubString(8, "12345678", true);
             Console.WriteLine($"{mutableString3} length is {mutableString3.Length} capacity is {mutableString3.Capacity}");
             // Displays 1234567812345678 length is 16 capacity is 32
+        }
+
+        static void Limitations()
+        {
+            Console.WriteLine("Limitations");
+            MutableString mutableString1 = new MutableString("AAAAAA");
+            MutableString mutableString2 = new MutableString("AAAAAA");
+            mutableString2.SetSubString(3, "DEF");
+            Console.WriteLine(mutableString1);
+            Console.WriteLine(mutableString2);
+            // Displays    AAADEF
+            //             AAADEF
         }
     }
 }
--- README.md
+++ README.md
@@ -14,19 +14,27 @@ In performance constrained environments such as video games garbage collection i
 
             Console.WriteLine($"{mutableString1}=={mutableString2} is {mutableString1 == mutableString2}");
             // Displays ABCDEF==AAAAAA is False
+
             mutableString2.SetSubString(3, "DEF");
             // Displays ABCDEF==AAADEF is False
+
             Console.WriteLine($"{mutableString1}=={mutableString2} is {mutableString1 == mutableString2}");
             mutableString2.SetSubString(0, "ABC", false);
-            // Displays ABCDEF==ABCDEF is True
             Console.WriteLine($"{mutableString1}=={mutableString2} is {mutableString1 == mutableString2}");
+            // Displays ABCDEF==ABCDEF is True
+
+            Console.WriteLine($"{mutableString2} length is {mutableString2.Length} capacity is {mutableString2.Capacity}");
+            // Displays ABCDEF length is 6 capacity is 6
+
             mutableString2.SetString("ABC");
-            Console.WriteLine($"{mutableString2} length is {mutableString2.Length}");
+            Console.WriteLine($"{mutableString2} length is {mutableString2.Length} capacity is {mutableString2.Capacity}");
             // Displays ABC length is 3
+
             MutableString mutableString3 = new MutableString(32);
             mutableString3.SetString("12345678");
             Console.WriteLine($"{mutableString3} length is {mutableString3.Length} capacity is {mutableString3.Capacity}");
             // Displays 12345678 length is 8 capacity is 32
+
             mutableString3.SetSubString(8, "12345678", true);
             Console.WriteLine($"{mutableString3} length is {mutableString3.Length} capacity is {mutableString3.Capacity}");
             // Displays 1234567812345678 length is 16 capacity is 32
@@ -44,13 +52,10 @@ This means if you instantiate two MutableStrings with the same intial string the
             mutableString2.SetSubString(3, "DEF");
             Console.WriteLine(mutableString1);
             Console.WriteLine(mutableString2);
-```
-```
-Console Output:
-> AAADEF
-> AAADEF
-```
+            // Displays    AAADEF
+            //             AAADEF
 
+```
 
 ## Requirements
 
